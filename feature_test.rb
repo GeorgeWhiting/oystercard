4c8dafb304dab02177fee33a43711254e44e9cfc
Tim Williamson <timwilliamson1337@gmail.com>
2017-11-27 17:37:24 +0000

    Adds deduct method

--- feature_test.rb
+++ feature_test.rb
@@ -3,5 +3,10 @@ p card = Oystercard.new
 p card.balance
 p card.top_up(20)
 p card.balance
-p card.top_up(100)
+begin 
+	card.top_up(100) 
+rescue Exception => e 
+	p e.message
+end
 p card.balance
+p card.deduct (15)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,10 +9,15 @@ describe Oystercard do
   end
   describe "#top_up" do
   	it "should accept a top up" do
-  	  expect(subject.top_up(20)).to eq(30)
+  	  expect(subject.top_up(20)).to eq(Oystercard::DEFAULT_STARTING_BALANCE + 20)
   	end
   	it "should not be able to go above limit" do
   	  expect{subject.top_up(100)}.to raise_error("Max £#{Oystercard::DEFAULT_LIMIT}, fool")
+  	end
+  end
+  describe "#deduct" do
+  	it "should deduct money" do
+  	  expect(subject.deduct(5)).to eq(Oystercard::DEFAULT_STARTING_BALANCE - 5) 
   	end
   end
 end
